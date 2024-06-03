5effac35849c9bebc4502b0ee6ced1379bae7a3c
Lucas Dias <lucasfdiasdev@gmail.com>
2024-06-03 02:38:49 -0300

    feat: add create folder task

---
+++ src/task/task.controller.ts
@@ -0,0 +1,12 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { TaskDto } from './taks.dto';
+import { TaskService } from './task.service';
+
+@Controller('task')
+export class TaskController {
+  constructor(private readonly tasksService: TaskService) {}
+  @Post()
+  create(@Body() task: TaskDto) {
+    this.tasksService.create(task);
+  }
+}
