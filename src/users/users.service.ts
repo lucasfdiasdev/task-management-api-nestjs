546e318fcd06677321a9c18e69e165dba9b0bbc7
Lucas Dias <lucasfdiasdev@gmail.com>
2024-06-03 16:59:14 -0300

    feat: generate module users

---
+++ src/users/users.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { UserDto } from './users.dto';
+import { v4 as uuid } from 'uuid';
+import { hashSync as bcrypt } from 'bcryptjs';
+
+@Injectable()
+export class UsersService {
+  private readonly users: UserDto[] = [];
+  create(newUser: UserDto) {
+    newUser.id = uuid();
+    newUser.password = bcrypt(newUser.password, 10);
+    this.users.push(newUser);
+    console.log(this.users);
+  }
+}
